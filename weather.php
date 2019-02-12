1f2fb3b29148de116dd697a6f745b46dbc0c2bac
Mahmud Jion <43998247+Quintus34@users.noreply.github.com>
2019-02-13 01:22:02 +0600

    Add files via upload

---
+++ weather.php
@@ -0,0 +1,21 @@
+<?php 
+$ip = '';
+$api = 'https://ipapi.co/' . $ip . '/latlong/';
+$location = file_get_contents($api);
+$point = explode(",", $location);
+
+$request= 'http://api.openweathermap.org/data/2.5/weather?lat=' . $point[0] . '&lon=' . $point[1] . '&appid=ca27e634bf28b4e2a1ddd86cc8dcc10d';
+$response = file_get_contents($request);
+$jsonobj = json_decode($response);
+$kelvin = $jsonobj->main->temp;  
+
+$celcius = $kelvin - 273.15; 
+
+$url = 'http://ip-api.com/json/?fields=city';
+$data = file_get_contents($url); 
+$characters = json_decode($data);
+
+echo "<br>$celcius";
+echo "<br>$characters->city";
+echo "<br>$location";
+?> 
